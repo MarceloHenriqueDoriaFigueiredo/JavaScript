a96eeb6aea1e74c683ecad7317c0228e8d8e6c40
Marcelo Figueiredo <marchenrique99@gmail.com>
2022-10-12 17:17:48 -0300

    Create media.js

---
+++ Exercicios/media.js
@@ -0,0 +1,12 @@
+const nota_1 = 10;
+const nota_2 = 7;
+const nota_3 = 8;
+const nota_4 = 6;
+
+const media = (nota_1 + nota_2 + nota_3 + nota_4)/4;
+
+if (media >= 6){
+    console.log("O aluno esta aprovado");
+} else{
+    console.log("O aluno não foi aprovado");
+};
