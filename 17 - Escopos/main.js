632fb09fe422e2ed019ad2c6b31c714202668c9b
Marcelo Figueiredo <marchenrique99@gmail.com>
2022-12-20 08:14:05 -0300

    estudos eventos teclado

---
+++ 17 - Escopos/main.js
@@ -0,0 +1,31 @@
+// if (true){
+//     const mensagem = 'Olá';
+//     // console.log(mensagem);
+// }
+// console.log(mensagem)
+
+
+// Bloco de código autonomo
+// {
+//     const mensagem = 'Olá';
+//     console.log(mensagem);
+// }
+
+
+// Escopo de função
+
+// function estudo(){
+//     const mensagem = 'Olá';
+//     console.log(mensagem);
+// }
+// estudo();
+
+// Escopo global
+let numero = 10;
+
+function ordem(){
+    const mensagem = 'Olá'
+    console.log(mensagem);
+}
+
+console.log(numero);
