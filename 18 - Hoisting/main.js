36b995d40df484a1b431c7307958960ccd58483b
Marcelo Figueiredo <marchenrique99@gmail.com>
2022-12-20 08:34:25 -0300

    Hoinsting

---
+++ 18 - Hoisting/main.js
@@ -0,0 +1,8 @@
+// Não importa onde as variaveis sejam declaradas dentro de um escopo
+// Elas sempre serão movidas para o topo do escopo seja ele global ou local
+// Ou seja as declarações das variaveis são inçadas para o topo
+// Para essa situação e somente movida a declaração
+
+let pais // Declarando
+pais = 'Brasil' // Atribuição
+console.log(pais)
