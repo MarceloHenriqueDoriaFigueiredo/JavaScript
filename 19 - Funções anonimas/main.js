46636fc7272f9f858d93f3cf391e0b8bb1a0d9a7
Marcelo Figueiredo <marchenrique99@gmail.com>
2022-12-20 08:55:30 -0300

    Funções anonimas

---
+++ 19 - Funções anonimas/main.js
@@ -0,0 +1,15 @@
+// As funções anonimas são funções que não são nomeadas.
+
+// Usando forma tradicional
+// function somar(numero1, numero2){
+//     return numero1 + numero2
+// }
+
+// let resultado = somar(10, 90);
+// console.log(resultado);
+
+// Usando função anonima
+let somatoria = function(numero1, numero2, numero3){
+    return numero1 + numero2 + numero3
+}
+console.log(somatoria(10, 20, 200))
