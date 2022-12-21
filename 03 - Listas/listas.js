694fbea61f306e3a7406b109a42591212d331bde
Marcelo Figueiredo <marchenrique99@gmail.com>
2022-12-21 08:22:58 -0300

    Slice e splice

--- 03 - Listas/listas.js
+++ 03 - Listas/listas.js
@@ -36,3 +36,20 @@ console.log(nomes.length);
 //Verificando se a variavel e uma lista
 const nomesInArray = Array.isArray(nomes);
 console.log(nomesInArray);
+
+// Usando o metodo slice para extrair dados de um array
+const lista = [1, 2, 3, 4, 5]
+let listaExtraida = lista.slice(0, 3);
+console.log(listaExtraida);
+
+// Usando splice para remover elementos de uma array atráves do indice
+const lista2 = [10, 20, 30, 40, 50];
+let listaExtraida2 = lista2.splice(1, 2);
+console.log(listaExtraida2);
+console.log(lista2);
+
+// Usanoo splice para remove e também adicionar elementos no array
+const list = [100, 200, 300, 400, true, false];
+let listExtraida = list.splice(1, 2, '200', '300', '400');
+console.log(listExtraida);
+console.log(list);
