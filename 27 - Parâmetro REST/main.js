b28792f53eb090c4df1f95889b92267e2a5eb2ba
Marcelo Figueiredo <marchenrique99@gmail.com>
2022-12-27 17:06:42 -0300

    Update main.js

--- 27 - Parâmetro REST/main.js
+++ 27 - Parâmetro REST/main.js
@@ -1,5 +1,5 @@
 // Com o parametro rest e nos permitido representa um numero indefinido de argumentos que iremos passar para uma função.
-// O paramentro rest sera interpretado com array em um função.
+// O paramentro rest sera interpretado como array em um função.
 
 function incentivo(mensagem, ...nomes){
     nomes.map(nomes => console.log(`${mensagem} ${nomes}`))
