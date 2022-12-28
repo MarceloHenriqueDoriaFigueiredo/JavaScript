a73ed390137ebec7a3141369bd0f43ea7688466e
Marcelo Figueiredo <marchenrique99@gmail.com>
2022-12-28 08:49:41 -0300

    async e await

---
+++ 32 - Async e Await/main.js
@@ -0,0 +1,2 @@
+// O async e o await nos ajudar a trabalhar com promises, com ele fica mais facil de trabalhar e não precisamos utilizar mais o then que agente usava antes quando trabalhava com promises puras.
+
