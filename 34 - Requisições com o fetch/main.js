b0571feb2108528ff558cff6c24cf2a59ddedba9
Marcelo Figueiredo <marchenrique99@gmail.com>
2023-01-04 17:36:30 -0300

    Update main.js

--- 34 - Requisições com o fetch/main.js
+++ 34 - Requisições com o fetch/main.js
@@ -8,13 +8,12 @@ async function criarBaralhoEmbaralhado(){
     const url = "https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
     const resposta = await fetch(url);
     return await resposta.json();
-}
+};
 
 async function tirarCartaDoBaralho(deck_id){
     const url = `https://www.deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`;
     const resposta = await fetch(url);
     return await resposta.json();
-
 };
 
 async function tirarUmaCartaDoBaralho(){
